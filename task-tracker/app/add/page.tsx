c5154ce0b190a4f7baf83fc72d0f769153722c71
Shrijit Vivek <shrijitv3@gmail.com>
2025-11-08 16:08:31 +0530

    taskform

--- task-tracker/app/add/page.tsx
+++ task-tracker/app/add/page.tsx
@@ -1,10 +1,10 @@
 import TaskForm from "../components/TaskForm"
 
-export default function AddTaskpage() {
+export default function AddTaskpage() { // taskform comp added which contains task creation logic
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Add New Task</h1>
-      <TaskForm/>
+      <TaskForm/>  
     </div>
   )
 }
